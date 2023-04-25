c373e7d29328e715bcb499ac20a4553791ee2e81
Junkogetsu <junkogetsu@gmail.com>
2023-04-25 22:53:23 +0200

    finalisation back pour profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\PasswordFormType;
 use App\Form\ProfilFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ProfilController extends AbstractController
 {
 
-    #[Route('/profil', name: 'profil_profil')]
+    #[Route('/profil', name: 'app_profil')]
     public function profil(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
@@ -24,25 +28,49 @@ class ProfilController extends AbstractController
         ]);
     }
 
-    #[Route('/profil/modifier', name: 'profil_modifier')]
-    public function modifier(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/profil/modifier', name: 'app_profil_modify')]
+    public function modifier(Request $request, EntityManagerInterface $entityManager, Security $security): Response
     {
-
-        //$user = new User();
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        $user = $this->getUser();
+        $user = $security->getUser();
         $form = $this->createForm(ProfilFormType::class, $user);
         $form->handleRequest($request);
 
         //traiter le formulaire
         if ($form->isSubmitted() && $form->isValid()) {
-
             $user = $form->getData();
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('profil_profil');
+            return $this->redirectToRoute('app_profil');
+        }
+
+        return $this->render('profil/modifier.html.twig', [
+            'profilForm' => $form->createView()
+        ]);
+    }
+    #[Route('/profil/mot-de-passe', name: 'app_profil_modify_password')]
+    public function modifierNom(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher, Security $security): Response
+    {
+        $user = $security->getUser();
+        $form = $this->createForm(PasswordFormType::class, $user);
+        $form->handleRequest($request);
+
+
+        //traiter le formulaire
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user= $form->getData();
+            $passwordHash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($passwordHash);
+
+            //^ tout ça ici v avec Symfony ^6.2
+            // $user = $form->getData();
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profil');
         }
 
         return $this->render('profil/modifier.html.twig', [
--- src/Form/PasswordFormType.php
+++ src/Form/PasswordFormType.php
@@ -6,31 +6,22 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProfilFormType extends AbstractType
+class PasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
 
         $builder
-            ->add('email', type: EmailType::class, options: [
-                'required' => true,
-            ])
-            ->add('pseudo', type: TextType::class, options: [
-                'required' => true,
-            ])
-            ->add('name', type: TextType::class, options: [
-                'required' => true,
-            ])
-            ->add('firstname', type: TextType::class, options: [
-                'required' => true,
-            ])
-            ->add('telephone', type: TextType::class, options: [
-                'required' => true,
+            ->add('password', type: RepeatedType::class, options: [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'Mot de passe'/*, 'hash_property_path' => 'password'*/],
+                'second_options' => ['label' => 'Confirmer mot de passe']
             ])
             ->add('save', type: SubmitType::class, options: [
                 'label' => 'Enregistrer'
